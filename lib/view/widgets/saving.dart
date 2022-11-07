7a9b7a213a6ef059911d833ee2d33fc21a8bdfc6
Nourah Alsaadan <nouraalsaadan@gmail.com>
2022-11-07 10:51:08 +0300

    updated the saving file

--- lib/view/widgets/saving.dart
+++ lib/view/widgets/saving.dart
@@ -1,10 +1,17 @@
 // ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:percent_indicator/percent_indicator.dart';
 
-class Saving extends StatelessWidget {
+class Saving extends StatefulWidget {
   const Saving({Key? key}) : super(key: key);
 
+  @override
+  State<Saving> createState() => _SavingState();
+}
+
+class _SavingState extends State<Saving> {
+  bool check = false; // to check if the button pressed or not
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -32,7 +39,34 @@ class Saving extends StatelessWidget {
                     color: Color.fromRGBO(0, 60, 79, 1),
                     size: 24,
                   ),
-                  onPressed: () {},
+                  onPressed: () => showDialog(
+                      context: context,
+                      builder: (BuildContext context) => AlertDialog(
+                            title: Text('add new goal'),
+                            content: Column(
+                              children: [
+                                TextFormField(
+                                  decoration: InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    hintText: 'goal name',
+                                  ),
+                                ),
+                                TextFormField(
+                                  decoration: InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    hintText: 'Amount',
+                                  ),
+                                ),
+                                ElevatedButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        check = false;
+                                      });
+                                    },
+                                    child: Text('hi')),
+                              ],
+                            ),
+                          )),
                 ),
               ),
               Text(
@@ -112,267 +146,37 @@ class Saving extends StatelessWidget {
                           ),
                         ),
                       ),
-                      ElevatedButton(onPressed: () {}, child: Text('add')),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              '500,000',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16,
-                                color: Color.fromRGBO(0, 60, 79, 1),
-                                //fontFamily:
-                              ),
-                            ),
-                            Row(
-                              // ignore: prefer_const_literals_to_create_immutables
+                      check == false
+                          ? ElevatedButton(
+                              onPressed: () {
+                                setState(() {
+                                  check = true;
+                                });
+                              },
+                              child: Text('add'))
+                          : Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
                               children: [
-                                Text(
-                                  'أهدافي',
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 16,
-                                    color: Color.fromRGBO(0, 60, 79, 1),
-                                    //fontFamily:
-                                  ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.only(left: 8.0),
-                                  child: Icon(
-                                    Icons.heart_broken,
-                                    color: Color.fromRGBO(0, 60, 79, 1),
-                                    size: 24,
+                                ElevatedButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        check = false;
+                                      });
+                                    },
+                                    child: Text('hi')),
+                                SizedBox(
+                                  width: 200,
+                                  child: TextField(
+                                    decoration: new InputDecoration(
+                                        labelText: "Enter your number"),
+                                    keyboardType: TextInputType.number,
+                                    inputFormatters: <TextInputFormatter>[
+                                      FilteringTextInputFormatter.digitsOnly
+                                    ], // Only numbers can be entered
                                   ),
                                 ),
                               ],
                             ),
-                          ],
-                        ),
-                      ),
-                      LinearPercentIndicator(
-                        animation: true,
-                        animationDuration: 2000,
-                        lineHeight: 20,
-                        percent: 0.7,
-                        barRadius: const Radius.circular(16),
-                        progressColor: Colors.blue[400],
-                        backgroundColor: Colors.grey[300],
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 16.0, top: 8),
-                        child: Align(
-                          alignment: Alignment.centerLeft,
-                          child: Text(
-                            '400,000',
-                          ),
-                        ),
-                      ),
-                      ElevatedButton(onPressed: () {}, child: Text('add')),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              '500,000',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16,
-                                color: Color.fromRGBO(0, 60, 79, 1),
-                                //fontFamily:
-                              ),
-                            ),
-                            Row(
-                              // ignore: prefer_const_literals_to_create_immutables
-                              children: [
-                                Text(
-                                  'أهدافي',
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 16,
-                                    color: Color.fromRGBO(0, 60, 79, 1),
-                                    //fontFamily:
-                                  ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.only(left: 8.0),
-                                  child: Icon(
-                                    Icons.heart_broken,
-                                    color: Color.fromRGBO(0, 60, 79, 1),
-                                    size: 24,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                      LinearPercentIndicator(
-                        animation: true,
-                        animationDuration: 2000,
-                        lineHeight: 20,
-                        percent: 0.7,
-                        barRadius: const Radius.circular(16),
-                        progressColor: Colors.blue[400],
-                        backgroundColor: Colors.grey[300],
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 16.0, top: 8),
-                        child: Align(
-                          alignment: Alignment.centerLeft,
-                          child: Text(
-                            '400,000',
-                          ),
-                        ),
-                      ),
-                      ElevatedButton(onPressed: () {}, child: Text('add')),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              '500,000',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16,
-                                color: Color.fromRGBO(0, 60, 79, 1),
-                                //fontFamily:
-                              ),
-                            ),
-                            Row(
-                              // ignore: prefer_const_literals_to_create_immutables
-                              children: [
-                                Text(
-                                  'أهدافي',
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 16,
-                                    color: Color.fromRGBO(0, 60, 79, 1),
-                                    //fontFamily:
-                                  ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.only(left: 8.0),
-                                  child: Icon(
-                                    Icons.heart_broken,
-                                    color: Color.fromRGBO(0, 60, 79, 1),
-                                    size: 24,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                      LinearPercentIndicator(
-                        animation: true,
-                        animationDuration: 2000,
-                        lineHeight: 20,
-                        percent: 0.7,
-                        barRadius: const Radius.circular(16),
-                        progressColor: Colors.blue[400],
-                        backgroundColor: Colors.grey[300],
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 16.0, top: 8),
-                        child: Align(
-                          alignment: Alignment.centerLeft,
-                          child: Text(
-                            '400,000',
-                          ),
-                        ),
-                      ),
-                      ElevatedButton(onPressed: () {}, child: Text('add')),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              '500,000',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16,
-                                color: Color.fromRGBO(0, 60, 79, 1),
-                                //fontFamily:
-                              ),
-                            ),
-                            Row(
-                              // ignore: prefer_const_literals_to_create_immutables
-                              children: [
-                                Text(
-                                  'أهدافي',
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 16,
-                                    color: Color.fromRGBO(0, 60, 79, 1),
-                                    //fontFamily:
-                                  ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.only(left: 8.0),
-                                  child: Icon(
-                                    Icons.heart_broken,
-                                    color: Color.fromRGBO(0, 60, 79, 1),
-                                    size: 24,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                      LinearPercentIndicator(
-                        animation: true,
-                        animationDuration: 2000,
-                        lineHeight: 20,
-                        percent: 0.7,
-                        barRadius: const Radius.circular(16),
-                        progressColor: Colors.blue[400],
-                        backgroundColor: Colors.grey[300],
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 16.0, top: 8),
-                        child: Align(
-                          alignment: Alignment.centerLeft,
-                          child: Text(
-                            '400,000',
-                          ),
-                        ),
-                      ),
-                      ElevatedButton(onPressed: () {}, child: Text('add')),
                     ],
                   ),
                 ),
