909c40193552d7886001bb96463f97b18c93e5e6
Nourah Alsaadan <nouraalsaadan@gmail.com>
2022-11-08 15:45:45 +0300

    updated the listview in the saving file

--- lib/view/widgets/saving.dart
+++ lib/view/widgets/saving.dart
@@ -11,8 +11,10 @@ import 'package:taqseemah/C/saving_controller.dart';
 
 import '../../M/records.dart';
 import '../../constance.dart';
+
 List<Records> MIFromJson(String str) =>
     List<Records>.from(json.decode(str).map((x) => Records.fromJson(x)));
+
 class Saving extends StatefulWidget {
   const Saving({Key? key}) : super(key: key);
 
@@ -22,7 +24,7 @@ class Saving extends StatefulWidget {
 
 class _SavingState extends State<Saving> {
   var Controller = Get.put(SavingController());
-  final _amuntController = TextEditingController()  ;
+  final _amuntController = TextEditingController();
   final _titleController = TextEditingController();
 
   loadRecords() async {
@@ -30,13 +32,9 @@ class _SavingState extends State<Saving> {
     setState(() {
       try {
         Controller.Saving = MIFromJson(prefs.getString("Saving")!);
-
-      } catch (e) {
-
-      }
+      } catch (e) {}
     });
   }
-
 
   bool check = false; // to check if the button pressed or not
   @override
@@ -61,68 +59,78 @@ class _SavingState extends State<Saving> {
             children: [
               Container(
                 child: IconButton(
-                  icon: Icon(
-                    Icons.add,
-                    color: Color.fromRGBO(0, 60, 79, 1),
-                    size: 24,
-                  ),
-                  onPressed: () =>    showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return AlertDialog(
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20)),
-                          content: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  " هدف جديد",
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 16,
-                                    color: Color.fromRGBO(0, 60, 79, 1),
-                                    //fontFamily:
+                    icon: Icon(
+                      Icons.add,
+                      color: Color.fromRGBO(0, 60, 79, 1),
+                      size: 24,
+                    ),
+                    onPressed: () => showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20)),
+                            content: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    " هدف جديد",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16,
+                                      color: Color.fromRGBO(0, 60, 79, 1),
+                                      //fontFamily:
+                                    ),
                                   ),
-                                ),
-                                SizedBox(height: 15,),
-                                TextFormField(
-                                  controller: _titleController,
-                                  decoration: InputDecoration(
-                                      border: OutlineInputBorder(
-                                        borderRadius: BorderRadius.circular(10.0),
-                                      ),
-                                      labelText: 'الهدف'),
-                                ),
-                                SizedBox(height: 15,),
-                                TextFormField(
-                                  controller: _amuntController,
-                                  keyboardType: TextInputType.number,
-                                  decoration: InputDecoration(
-                                      border: OutlineInputBorder(
-                                        borderRadius: BorderRadius.circular(10.0),
-                                      ),
-                                      labelText: 'المبلغ'),
-                                ),
-                                SizedBox(height: 15,)
-                              ]),
-                          actions: <Widget>[
-                            ElevatedButton(onPressed: () {
-                              Controller.add(context,_titleController.text,  int.parse(_amuntController.text),);
-                              _titleController.clear();
-                              _amuntController.clear();
-                              Get.back();
-
-
-                            }, child: Text('إضافة')),
-                            TextButton(
-                                onPressed: Navigator.of(context).pop,
-                                child: const Text('إلغاء')),
-                          ],
-                        );
-                      })
-
-                ),
+                                  SizedBox(
+                                    height: 15,
+                                  ),
+                                  TextFormField(
+                                    controller: _titleController,
+                                    decoration: InputDecoration(
+                                        border: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(10.0),
+                                        ),
+                                        labelText: 'الهدف'),
+                                  ),
+                                  SizedBox(
+                                    height: 15,
+                                  ),
+                                  TextFormField(
+                                    controller: _amuntController,
+                                    keyboardType: TextInputType.number,
+                                    decoration: InputDecoration(
+                                        border: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(10.0),
+                                        ),
+                                        labelText: 'المبلغ'),
+                                  ),
+                                  SizedBox(
+                                    height: 15,
+                                  )
+                                ]),
+                            actions: <Widget>[
+                              ElevatedButton(
+                                  onPressed: () {
+                                    Controller.add(
+                                      context,
+                                      _titleController.text,
+                                      int.parse(_amuntController.text),
+                                    );
+                                    _titleController.clear();
+                                    _amuntController.clear();
+                                    Get.back();
+                                  },
+                                  child: Text('إضافة')),
+                              TextButton(
+                                  onPressed: Navigator.of(context).pop,
+                                  child: const Text('إلغاء')),
+                            ],
+                          );
+                        })),
               ),
               Text(
                 'أهدافي',
@@ -136,45 +144,143 @@ class _SavingState extends State<Saving> {
             ],
           ),
           SizedBox(
-            height: 350,
-              child:   ListView.builder(
-                // return ListView.builder(
+              height: 350,
+              child: ListView.builder(
+                  // return ListView.builder(
                   shrinkWrap: true,
                   itemCount: Controller.Saving.length,
                   itemBuilder: (BuildContext context, int position) {
-                    return Card(
-                      margin: const EdgeInsets.all(8),
-                      elevation: 2.4,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20)),
-                      child: ListTile(
-                        title: Text(Controller.Saving[position].title),
-                        subtitle: Text(Controller.Saving[position].amount.toString()),
-                        trailing: IconButton(
-                          icon: const Icon(
-                            Icons.delete,
-                            color: DarkColor1,
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  '${Controller.Saving[position].amount.toString()}',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16,
+                                    color: Color.fromRGBO(0, 60, 79, 1),
+                                    //fontFamily:
+                                  ),
+                                ),
+                                Row(
+                                  // ignore: prefer_const_literals_to_create_immutables
+                                  children: [
+                                    Text(
+                                      '${Controller.Saving[position].title}',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16,
+                                        color: Color.fromRGBO(0, 60, 79, 1),
+                                        //fontFamily:
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.only(left: 8.0),
+                                      child: Icon(
+                                        Icons.heart_broken,
+                                        color: Color.fromRGBO(0, 60, 79, 1),
+                                        size: 24,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
                           ),
-                          onPressed: () {
-                            setState(() {
-                              Controller.delete(context, position);
-                            });
-                          },
-                        ),
+                          LinearPercentIndicator(
+                            animation: true,
+                            animationDuration: 2000,
+                            lineHeight: 20,
+                            percent: 0.7,
+                            barRadius: const Radius.circular(16),
+                            progressColor: Colors.blue[400],
+                            backgroundColor: Colors.grey[300],
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 16.0, top: 8),
+                            child: Align(
+                              alignment: Alignment.centerLeft,
+                              child: Text(
+                                '400,000',
+                              ),
+                            ),
+                          ),
+                          check == false
+                              ? ElevatedButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      check = true;
+                                    });
+                                  },
+                                  child: Text('add'))
+                              : Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    ElevatedButton(
+                                        onPressed: () {
+                                          setState(() {
+                                            check = false;
+                                          });
+                                        },
+                                        child: Text('hi')),
+                                    SizedBox(
+                                      width: 200,
+                                      child: TextField(
+                                        decoration: new InputDecoration(
+                                            labelText: "Enter your number"),
+                                        keyboardType: TextInputType.number,
+                                        inputFormatters: <TextInputFormatter>[
+                                          FilteringTextInputFormatter.digitsOnly
+                                        ], // Only numbers can be entered
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                        ],
                       ),
-
                     );
                   })
-            // child: ListView(
+              // child: ListView(
 
-          ),
+              ),
         ],
       ),
     );
   }
+
   @override
   void initState() {
     super.initState();
     loadRecords();
   }
 }
+
+
+// Card(
+//                       margin: const EdgeInsets.all(8),
+//                       elevation: 2.4,
+//                       shape: RoundedRectangleBorder(
+//                           borderRadius: BorderRadius.circular(20)),
+//                       child: ListTile(
+//                         title: Text(Controller.Saving[position].title),
+//                         subtitle: Text(Controller.Saving[position].amount.toString()),
+//                         trailing: IconButton(
+//                           icon: const Icon(
+//                             Icons.delete,
+//                             color: DarkColor1,
+//                           ),
+//                           onPressed: () {
+//                             setState(() {
+//                               Controller.delete(context, position);
+//                             });
+//                           },
+//                         ),
+//                       ),
+
+//                     );
